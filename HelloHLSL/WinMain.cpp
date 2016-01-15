a11b9c28ff2f27dcf55b3496da6209a9fea2ef43
PillowSky <pillowsky@qq.com>
2016-01-16 00:27:28 +0800

    first triangle

--- HelloHLSL/WinMain.cpp
+++ HelloHLSL/WinMain.cpp
@@ -9,17 +9,30 @@
 #include "VertexShader.h"
 #include "PixelShader.h"
 
-#pragma warning( disable : 4100 )
+#pragma warning(disable : 4100)
 
 using namespace DirectX;
 
+//--------------------------------------------------------------------------------------
+// Structures
+//--------------------------------------------------------------------------------------
+struct SimpleVertex {
+	XMFLOAT3 Pos;
+};
+
+//--------------------------------------------------------------------------------------
+// Global Variables
+//--------------------------------------------------------------------------------------
+ID3D11VertexShader*         g_pVertexShader = nullptr;
+ID3D11PixelShader*          g_pPixelShader = nullptr;
+ID3D11InputLayout*          g_pVertexLayout = nullptr;
+ID3D11Buffer*               g_pVertexBuffer = nullptr;
 
 //--------------------------------------------------------------------------------------
 // Reject any D3D11 devices that aren't acceptable by returning false
 //--------------------------------------------------------------------------------------
 bool CALLBACK IsD3D11DeviceAcceptable(const CD3D11EnumAdapterInfo *AdapterInfo, UINT Output, const CD3D11EnumDeviceInfo *DeviceInfo,
-	DXGI_FORMAT BackBufferFormat, bool bWindowed, void* pUserContext)
-{
+	DXGI_FORMAT BackBufferFormat, bool bWindowed, void* pUserContext) {
 	return true;
 }
 
@@ -27,8 +40,7 @@ bool CALLBACK IsD3D11DeviceAcceptable(const CD3D11EnumAdapterInfo *AdapterInfo, 
 //--------------------------------------------------------------------------------------
 // Called right before creating a D3D device, allowing the app to modify the device settings as needed
 //--------------------------------------------------------------------------------------
-bool CALLBACK ModifyDeviceSettings(DXUTDeviceSettings* pDeviceSettings, void* pUserContext)
-{
+bool CALLBACK ModifyDeviceSettings(DXUTDeviceSettings* pDeviceSettings, void* pUserContext) {
 	return true;
 }
 
@@ -36,10 +48,67 @@ bool CALLBACK ModifyDeviceSettings(DXUTDeviceSettings* pDeviceSettings, void* pU
 //--------------------------------------------------------------------------------------
 // Create any D3D11 resources that aren't dependant on the back buffer
 //--------------------------------------------------------------------------------------
-HRESULT CALLBACK OnD3D11CreateDevice(ID3D11Device* pd3dDevice, const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc,
-	void* pUserContext)
-{
-	return S_OK;
+HRESULT CALLBACK OnD3D11CreateDevice(ID3D11Device* pd3dDevice, const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext) {
+	HRESULT hr = S_OK;
+
+	auto pd3dImmediateContext = DXUTGetD3D11DeviceContext();
+
+	DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#ifdef _DEBUG
+	// Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
+	// Setting this flag improves the shader debugging experience, but still allows 
+	// the shaders to be optimized and to run exactly the way they will run in 
+	// the release configuration of this program.
+	dwShaderFlags |= D3DCOMPILE_DEBUG;
+
+	// Disable optimizations to further improve shader debugging
+	dwShaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+	// Create the vertex shader
+	V_RETURN(pd3dDevice->CreateVertexShader(g_vertexshader_code, sizeof(g_vertexshader_code), nullptr, &g_pVertexShader));
+
+	// Define the input layout
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+	V_RETURN(pd3dDevice->CreateInputLayout(layout, ARRAYSIZE(layout), g_vertexshader_code, sizeof(g_vertexshader_code), &g_pVertexLayout));
+	pd3dImmediateContext->IASetInputLayout(g_pVertexLayout);
+
+	// Create the pixel shader
+	V_RETURN(pd3dDevice->CreatePixelShader(g_pixelshader_code, sizeof(g_pixelshader_code), nullptr, &g_pPixelShader));
+
+	// Create vertex buffer
+	SimpleVertex vertices[] =
+	{
+		XMFLOAT3(0.0f, 0.5f, 0.5f),
+		XMFLOAT3(0.5f, -0.5f, 0.5f),
+		XMFLOAT3(-0.5f, -0.5f, 0.5f),
+	};
+
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(SimpleVertex) * 3;
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	
+	D3D11_SUBRESOURCE_DATA InitData;
+	ZeroMemory(&InitData, sizeof(InitData));
+	InitData.pSysMem = vertices;
+
+	V_RETURN(pd3dDevice->CreateBuffer(&bd, &InitData, &g_pVertexBuffer));
+
+	// Set vertex buffer
+	UINT stride = sizeof(SimpleVertex);
+	UINT offset = 0;
+	pd3dImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+
+	// Set primitive topology
+	pd3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	return hr;
 }
 
 
@@ -47,8 +116,7 @@ HRESULT CALLBACK OnD3D11CreateDevice(ID3D11Device* pd3dDevice, const DXGI_SURFAC
 // Create any D3D11 resources that depend on the back buffer
 //--------------------------------------------------------------------------------------
 HRESULT CALLBACK OnD3D11ResizedSwapChain(ID3D11Device* pd3dDevice, IDXGISwapChain* pSwapChain,
-	const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext)
-{
+	const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext) {
 	return S_OK;
 }
 
@@ -56,39 +124,42 @@ HRESULT CALLBACK OnD3D11ResizedSwapChain(ID3D11Device* pd3dDevice, IDXGISwapChai
 //--------------------------------------------------------------------------------------
 // Handle updates to the scene.  This is called regardless of which D3D API is used
 //--------------------------------------------------------------------------------------
-void CALLBACK OnFrameMove(double fTime, float fElapsedTime, void* pUserContext)
-{
+void CALLBACK OnFrameMove(double fTime, float fElapsedTime, void* pUserContext) {
 }
 
 
 //--------------------------------------------------------------------------------------
 // Render the scene using the D3D11 device
 //--------------------------------------------------------------------------------------
-void CALLBACK OnD3D11FrameRender(ID3D11Device* pd3dDevice, ID3D11DeviceContext* pd3dImmediateContext,
-	double fTime, float fElapsedTime, void* pUserContext)
-{
+void CALLBACK OnD3D11FrameRender(ID3D11Device* pd3dDevice, ID3D11DeviceContext* pd3dImmediateContext, double fTime, float fElapsedTime, void* pUserContext) {
 	// Clear render target and the depth stencil 
 	auto pRTV = DXUTGetD3D11RenderTargetView();
 	pd3dImmediateContext->ClearRenderTargetView(pRTV, Colors::MidnightBlue);
 
 	auto pDSV = DXUTGetD3D11DepthStencilView();
 	pd3dImmediateContext->ClearDepthStencilView(pDSV, D3D11_CLEAR_DEPTH, 1.0, 0);
+
+	pd3dImmediateContext->VSSetShader(g_pVertexShader, NULL, 0);
+	pd3dImmediateContext->PSSetShader(g_pPixelShader, NULL, 0);
+	pd3dImmediateContext->Draw(3, 0);
 }
 
 
 //--------------------------------------------------------------------------------------
 // Release D3D11 resources created in OnD3D11ResizedSwapChain 
 //--------------------------------------------------------------------------------------
-void CALLBACK OnD3D11ReleasingSwapChain(void* pUserContext)
-{
+void CALLBACK OnD3D11ReleasingSwapChain(void* pUserContext) {
 }
 
 
 //--------------------------------------------------------------------------------------
 // Release D3D11 resources created in OnD3D11CreateDevice 
 //--------------------------------------------------------------------------------------
-void CALLBACK OnD3D11DestroyDevice(void* pUserContext)
-{
+void CALLBACK OnD3D11DestroyDevice(void* pUserContext) {
+	SAFE_RELEASE(g_pVertexShader);
+	SAFE_RELEASE(g_pPixelShader);
+	SAFE_RELEASE(g_pVertexLayout);
+	SAFE_RELEASE(g_pVertexBuffer);
 }
 
 
@@ -96,8 +167,7 @@ void CALLBACK OnD3D11DestroyDevice(void* pUserContext)
 // Handle messages to the application
 //--------------------------------------------------------------------------------------
 LRESULT CALLBACK MsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
-	bool* pbNoFurtherProcessing, void* pUserContext)
-{
+	bool* pbNoFurtherProcessing, void* pUserContext) {
 	return 0;
 }
 
@@ -105,8 +175,7 @@ LRESULT CALLBACK MsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
 //--------------------------------------------------------------------------------------
 // Handle key presses
 //--------------------------------------------------------------------------------------
-void CALLBACK OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext)
-{
+void CALLBACK OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext) {
 }
 
 
@@ -115,16 +184,14 @@ void CALLBACK OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void* pUserCo
 //--------------------------------------------------------------------------------------
 void CALLBACK OnMouse(bool bLeftButtonDown, bool bRightButtonDown, bool bMiddleButtonDown,
 	bool bSideButton1Down, bool bSideButton2Down, int nMouseWheelDelta,
-	int xPos, int yPos, void* pUserContext)
-{
+	int xPos, int yPos, void* pUserContext) {
 }
 
 
 //--------------------------------------------------------------------------------------
 // Call if device was removed.  Return true to find a new device, false to quit
 //--------------------------------------------------------------------------------------
-bool CALLBACK OnDeviceRemoved(void* pUserContext)
-{
+bool CALLBACK OnDeviceRemoved(void* pUserContext) {
 	return true;
 }
 
@@ -132,8 +199,7 @@ bool CALLBACK OnDeviceRemoved(void* pUserContext)
 //--------------------------------------------------------------------------------------
 // Initialize everything and go into a render loop
 //--------------------------------------------------------------------------------------
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
-{
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
 	// Enable run-time memory check for debug builds.
 #ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
@@ -165,12 +231,10 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	DXUTCreateWindow(L"HelloHLSL");
 
 	// Only require 10-level hardware or later
-	DXUTCreateDevice(D3D_FEATURE_LEVEL_10_0, true, 800, 600);
+	DXUTCreateDevice(D3D_FEATURE_LEVEL_11_0, true, 800, 600);
 	DXUTMainLoop(); // Enter into the DXUT ren  der loop
 
 					// Perform any application-level cleanup here
 
 	return DXUTGetExitCode();
 }
-
-
